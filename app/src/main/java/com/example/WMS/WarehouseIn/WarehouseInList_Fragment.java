0ea80a9a5e5bb803a152c9023be4758b04e21b0b
forlawa <845422138@qq.com>
2021-02-03 17:19:36 +0800

    改用RecycleView

--- app/src/main/java/com/example/WMS/WarehouseIn/WarehouseInList_Fragment.java
+++ app/src/main/java/com/example/WMS/WarehouseIn/WarehouseInList_Fragment.java
@@ -18,15 +18,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.WMS.R;
 import com.example.WMS.domain.WarehouseItem;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WarehouseInList_Fragment extends Fragment{
     protected Context context;
-    private ListView lv_video_pager;
+    private RecyclerView lv_video_pager;
     private TextView tv_nomedia;
     private ProgressBar pb_loading;
     private ArrayList<WarehouseItem> warehouseItems;
@@ -44,6 +47,7 @@ public class WarehouseInList_Fragment extends Fragment{
     public View initView(){
         View view=View.inflate(context,R.layout.inlist_fragment,null);
         lv_video_pager=view.findViewById(R.id.lv_video_pager);
+        lv_video_pager.setLayoutManager(new LinearLayoutManager(context));
         tv_nomedia=view.findViewById(R.id.tv_nomedia);
         pb_loading=view.findViewById(R.id.pb_loading);
         return view;
@@ -66,7 +70,7 @@ public class WarehouseInList_Fragment extends Fragment{
                 //假数据
                 warehouseItem.setName("仓库1");
                 warehouseItems.add(warehouseItem);
-                warehouseItem.setName("仓库1");
+                warehouseItem.setName("仓库2");
                 warehouseItems.add(warehouseItem);
                 handler.sendEmptyMessage(0);
             }
@@ -80,7 +84,8 @@ public class WarehouseInList_Fragment extends Fragment{
                 tv_nomedia.setVisibility(View.GONE);
                 pb_loading.setVisibility(View.GONE);
                 //设置适配器
-                lv_video_pager.setAdapter(new WarehouseInList_Fragment.WarehouseInListAdapter());
+
+                lv_video_pager.setAdapter(new WarehouseInList_Fragment.WarehouseInListAdapter(warehouseItems));
             }
             else{
                 tv_nomedia.setVisibility(View.VISIBLE);
@@ -88,44 +93,43 @@ public class WarehouseInList_Fragment extends Fragment{
             }
         }
     };
-    static class ViewHolder{
-        ImageView iv_icon;
-        TextView tv_name;
-    }
-    class WarehouseInListAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return warehouseItems.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            WarehouseInList_Fragment.ViewHolder viewHolder;
-            if(view==null){
-                view=View.inflate(context, R.layout.item_inlist,null);
-                viewHolder=new WarehouseInList_Fragment.ViewHolder();
-                viewHolder.iv_icon=view.findViewById(R.id.iv_icon);
-                viewHolder.tv_name=view.findViewById(R.id.tv_name);
-                view.setTag(viewHolder);
+    public class WarehouseInListAdapter extends RecyclerView.Adapter<WarehouseInListAdapter.VH>{
+        //② 创建ViewHolder
+        public class VH extends RecyclerView.ViewHolder{
+            public final TextView title;
+            public VH(View v) {
+                super(v);
+                title = (TextView) v.findViewById(R.id.tv_name);
             }
-            else{
-                viewHolder = (WarehouseInList_Fragment.ViewHolder) view.getTag();
-            }
-            WarehouseItem warehouseItem=warehouseItems.get(i);
-            viewHolder.tv_name.setText(warehouseItem.getName());
-            //viewHolder.iv_icon.setImageResource(R.drawable.music_default_bg);
-            return view;
+        }
+
+        private ArrayList<WarehouseItem> mDatas;
+        public WarehouseInListAdapter(ArrayList<WarehouseItem> data) {
+            this.mDatas = data;
+        }
+
+        //③ 在Adapter中实现3个方法
+        @Override
+        public void onBindViewHolder(VH holder, int position) {
+            holder.title.setText(mDatas.get(position).getName());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //item 点击事件
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        @Override
+        public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+            //LayoutInflater.from指定写法
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_inlist, parent, false);
+            return new VH(v);
         }
     }
 }
